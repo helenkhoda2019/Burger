1c16edbbf648334eb6a4cd925fb1fe189af453b2
Helen Khoda <helen_khoda@yahoo.com>
2019-07-24 17:14:20 -0500

    final

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@
  var mysql = require('mysql');
  var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
+    port: process.env.PORT || 3306,
+    user: "",
+    password: "",
    database: "burger_db"
   });
   
